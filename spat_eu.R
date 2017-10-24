8b074018dacdf166a0f13922e23d17630b8de6c7
agsinkevich <agsinkevich@vm-as337>
2017-10-24 19:24:49 +0300

    changes

--- spat_eu.R
+++ spat_eu.R
@@ -1,25 +1,30 @@
 
+
+
+library(readr)
+electricity_prices_non_household <- read_csv("electricity_prices_non_household.csv")
+
+
 library(readxl)
-
-electricity_consumption <- read_excel("spatial_project/electricity_consumption.xls",skip = 10)
-
-
+employment_EU <- read_excel("employment_EU.xls")
 
 
 #working with shapefile
 
 require(rgdal)
 # Read SHAPEFILE.shp from the current working directory (".")
-shape <- readOGR(dsn="/students/agsinkevich/spatial_project/CNTR_RG_01M_2013.shp")
+shape <- readOGR(dsn="/students/agsinkevich/28_10_exam_spatial_econometrics/CNTR_RG_01M_2013.shp")
 
-setwd("/students/agsinkevich/spatial_project")
+setwd("/students/agsinkevich/28_10_exam_spatial_econometrics")
 
-EU_abbreviations <- read_excel("EU_abbreviations.xlsx", col_names = c("GEO/TIME","Short"))
+EU_abbreviations <- read_excel("EU_abbreviations.xlsx", col_names = c("GEO","Short"))
 
 
 #plotting
 
+png("EU.png", width = 1800, height = 1600)
 plot(shape)
+dev.off()
 
 summary(shape)
 
@@ -32,5 +37,7 @@ coord<-coordinates(shape)
 
 b <- knn2nb(knearneigh(coord, k = 1), row.names = shape$CNTR_ID)
 
+png("EU.png", width = 1800, height = 1600)
 plot(b, coord, add=TRUE)
+dev.off()
 
