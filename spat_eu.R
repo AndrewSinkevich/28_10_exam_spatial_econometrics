053ed1830ebd9211e76a1cb63808f0e5259f3d0f
agsinkevich <agsinkevich@vm-as337>
2017-10-28 08:32:20 +0300

    final version of spatial project

--- spat_eu.R
+++ spat_eu.R
@@ -70,15 +70,20 @@ class(x$TIME)
 
 #x<-filter(x, GEO==y$GEO)
 
-myvars <- levels(x$GEO) %in% levels(y$GEO)
+#myvars <- levels(x$GEO) %in% levels(y$GEO)
 
-xx$GEO <- subset(xx, myvars)
+#xx$GEO <- subset(xx, myvars)
+c<-levels(y$GEO)
+class(c)
+c<-as.vector.factor(c)
 
-xx<-subset(x, x$GEO %in% y$GEO)
+x<-x[x$GEO %in% c,]
 
-x<-subset(x, y$GEO %in% x$GEO)
-levels(xx$GEO)
-levels(y$GEO)
+#x<-subset(x, y$GEO %in% x$GEO)
+#na.omit(xx)
+
+tail(group_by(x, GEO) %>% summarise(count=n()))
+
 
 library(tidyr)
 y<-gather(y, key = TIME, value = Employment_rate, 2:63)
@@ -97,6 +102,10 @@ class(y$TIME)
 #Modify the data
 y$Employment_rate<-as.numeric(y$Employment_rate)
 
+y<-y[y$GEO %in% x$GEO,]
+
+group_by(y, GEO) %>% summarise(count=n())
+
 library(stats)
 plot.ts()
 
@@ -113,22 +122,31 @@ library(ggmap)
 
 # it was really tough
 
-shape_new<-shape[shape$CNTR_ID %in% levels(y$GEO) ,]
+shape_new<-shape[shape$CNTR_ID %in% y$GEO ,]
 
 shape_new$CNTR_ID<-na.omit(shape_new$CNTR_ID)
  
 plot(shape_new)
 shape_new$CNTR_ID
+
+
+coord<-coordinates(shape_new)
+
+
+distance_10 <- knn2nb(knearneigh(coord, k = 10), row.names = shape_new$CNTR_ID)
+
+plot(distance_10,  coord, add=TRUE)
 # plotting a map emplyment rate (to have visible borders) (Milano)
 
 shape_new$CNTR_ID
+
 
 c<-levels(y$GEO)
 
 levels(x$GEO)
 #shape_new<-filter(shape, CNTR_ID %in% y$GEO)
 
-df<-merge(shape_new, y,  by.x="CNTR_ID" , by.y="GEO")
+df<-merge(y,  shape_new, by.x="GEO" , by.y="CNTR_ID")
 
 ggplot(shape_new, aes(long, lat))+   geom_polygon(aes(x=long, y=lat, group=group, fill = Employment_rate)) + coord_equal()
 #m2 <- m1 + geom_path(aes(x=long, y=lat, group=group), color='black')
@@ -149,5 +167,89 @@ ggplot(shape_new, aes(long, lat))+   geom_polygon(aes(x=long, y=lat, group=group
 
 #Moran's I
 
+#we choose 2012 year in panel data to observe
 
-#Moran's scatterplot
+x_1<-filter(x, TIME=="2012-01-01" & CURRENCY=="EUR" & TAX=="All taxes and levies included") # 1 semester
+
+x_2<-filter(x, TIME=="2012-07-01" & CURRENCY=="EUR" & TAX=="All taxes and levies included") # 2 semester
+
+
+y_1<-filter(y,TIME=="2012-01-01") # 1 semester
+y_1<-y_1[y_1$GEO %in% x_1$GEO,]
+
+y_2<- filter(y,TIME=="2012-07-01") # 2 semester
+y_2<-y_2[y_2$GEO %in% x_2$GEO,]
+
+#again develop shapefile with only 30 countries 
+
+shape_new<-shape[shape$CNTR_ID %in% y_1$GEO ,]
+
+shape_new$CNTR_ID<-na.omit(shape_new$CNTR_ID)
+
+plot(shape_new)
+shape_new$CNTR_ID
+
+
+coord<-coordinates(shape_new)
+
+
+distance_10 <- knn2nb(knearneigh(coord, k = 10), row.names = shape_new$CNTR_ID)
+
+w<-nb2listw(distance_10, style='B')
+
+plot(distance_10,  coord, add=TRUE)
+
+sort.list(x_1$GEO)
+
+min(x_1$Value)
+max(x_1$Value)
+
+min(y_1$Employment_rate)
+max(y_1$Employment_rate)
+
+#Moran test for electricity price 
+moran.test(x_1$Value, w)
+
+
+moran.test(x_2$Value, w)
+
+#Moran's scatterplot for electricity
+moran.plot(x_1$Value, w, zero.policy=NULL, spChk=NULL, labels=c,
+           xlab= "Price for electricity 1 semester", ylab="Spatially lagged price for electricity 1 semester")
+
+
+
+
+#Moran test for employment
+moran.test(y_1$Employment_rate, w)
+
+moran.test(y_2$Employment_rate, w)
+
+
+#Moran's scatterplot for employment
+
+moran.plot(y_1$Employment_rate, w, zero.policy=NULL, spChk=NULL, labels=c,
+           xlab= "Employment 1 semester", ylab="Spatially lagged employment 1 semester")
+
+
+
+#SAR
+df_main_1<-inner_join(x_1, y_1, by = "GEO")
+
+sar.y_1<-lagsarlm(Employment_rate~ Value, data=df_main_1, listw=w)
+summary(sar.y_1)
+
+df_main_2<-inner_join(x_2, y_2, by = "GEO")
+
+sar.y_2<-lagsarlm(Employment_rate~ Value, data=df_main_2, listw=w)
+summary(sar.y_2)
+
+
+
+sar.y_1<-lagsarlm(Employment_rate~ Value, data=df_main_1, listw=w, type="Durbin")
+summary(sar.y_1)
+
+
+
+sar.y_2<-lagsarlm(Employment_rate~ Value, data=df_main_2, listw=w, type="Durbin")
+summary(sar.y_2)
